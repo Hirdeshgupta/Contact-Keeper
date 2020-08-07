4478a795b72749f400a72754025f2e652baf2329
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-08-07 14:38:13 +0530

    Routes protected and login add

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Router = express.Router();
 const auth = require("../middleware/auth");
+const Users = require("../models/user");
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require("express-validator")
+const jwt = require("jsonwebtoken");
+
 
 // @route api/auth
 //@method GET
@@ -16,8 +21,36 @@ Router.get("/", auth, (req, res) => {
 //@desrc  To authenticate the users  
 //@privacy public
 
-Router.post("/", (req, res) => {
-    res.json({ msg: "Authenticate the users " })
+Router.post("/", [
+    body('email', 'Please enter valid e-mail').not().isEmpty().isEmail(),
+    body('password', 'Please enter password').not().isEmpty()
+], async(req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+    //check whether the email already exists or not 
+    const is_email = await Users.findOne({ email });
+    if (!is_email) {
+        errors.errors.push({ msg: "This e-mail is not registered try registering first " })
+    } else {
+        if (password) {
+            const comparedPass = await bcrypt.compare(password, is_email.password);
+            if (!comparedPass) errors.errors.push({ msg: "Invalid Credentials" })
+        }
+    }
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const token = await jwt.sign({
+            data: {
+                id: is_email._id
+            }
+        }, process.env.JSON_URI, { expiresIn: 60 * 60 })
+        console.log(token)
+        return res.json({ token: token })
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 
